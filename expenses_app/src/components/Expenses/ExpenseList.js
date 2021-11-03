54a11748e815c8fbb1359f53a19b4cfe0e67789e
Nicole <nicole.barnett.nz@gmail.com>
2021-11-04 11:43:17 +1300

    reorganising folder structure

--- expenses_app/src/components/Expenses/ExpenseList.js
+++ expenses_app/src/components/Expenses/ExpenseList.js
@@ -1,9 +1,10 @@
 import "./ExpenseList.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card"
 
 const ExpenseList = (props) => {
   return (
-    <div className="expense-list">
+    <Card className="expense-list">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -24,7 +25,7 @@ const ExpenseList = (props) => {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 };
 
