c976151c53ee63a15ddfb1e739b5e7422b9dedb4
Nicole <nicole.barnett.nz@gmail.com>
2021-11-04 09:27:06 +1300

    adding css to ExpenseItem

--- expenses_app/src/components/ExpenseItem.js
+++ expenses_app/src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
+import './ExpenseItem.css';
+
 const ExpenseItem = () => {
+    const expenseDate = new Date(2021, 11, 4);
+    const expenseTitle = "Groceries";
+    const expenseAmount = 300.00;
+    
     return (
-    <div>
-        <div>Date</div>
-        <div>
-            <h2>Title</h2>
-            <div>Amount</div>
+
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
+            </div>
         </div>
-    </div>
     )
 };
 
