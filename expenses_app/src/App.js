6bfed999a65560517183992af5913b06b88574e4
Nicole <nicole.barnett.nz@gmail.com>
2021-12-07 17:24:05 +1300

    adding stateful list

--- expenses_app/src/App.js
+++ expenses_app/src/App.js
@@ -1,45 +1,45 @@
-import { ExpenseList } from "./components/Expenses/ExpenseList";
-import { NewExpense } from "./components/NewExpenses.js/NewExpense";
+import React, { useState } from 'react';
 
-const expenses = [
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
   {
-    id: "e1",
-    title: "Food for Grigi",
-    amount: 7.00,
-    date: new Date(2021, 10, 16)
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
   },
   {
-    id: "e2",
-    title: "Cotto Dinner",
-    amount: 135.00,
-    date: new Date(2021, 9, 10)
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
   },
-  {
-    id: "e3",
-    title: "Rollerskates",
-    amount: 85.00,
-    date: new Date(2021, 5, 27)
-  },
-  {
-    id: "e4",
-    title: "Birthday Cake",
-    amount: 100.00,
-    date: new Date(2021, 7, 26)
-  }
 ];
 
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = (expenseData) => {
-    console.log(expenseData);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
     <div>
-        <NewExpense onAddExpense={addExpenseHandler}/>
-        <ExpenseList items={expenses}/>  
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
