9a6bf647f292abb978ace325f5bad5cbfdc38c32
Nicole <nicole.barnett.nz@gmail.com>
2021-12-12 11:50:56 +1300

    adding filter funtionality and keys

--- expenses_app/src/App.js
+++ expenses_app/src/App.js
@@ -6,21 +6,25 @@ import Expenses from './components/Expenses/Expenses';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
+    title: 'Birthday Cake',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2',
+    title: 'Cat Food', 
+    amount: 7.49, 
+    date: new Date(2021, 2, 12) 
+  },
   {
     id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
+    title: 'Roller Skates',
+    amount: 94.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
+    title: 'Salad',
+    amount: 4.50,
     date: new Date(2021, 5, 12),
   },
 ];
